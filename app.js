5419d0c5e565bf39ea0f3f78412825d12c0e01c6
Ivash1315 <gertrude.atah@gmail.com>
2022-07-07 02:22:32 -0400

    slowly but steady

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to the Ivash World'));
+app.get('/', (req, res) => res.send('slowly but steady'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
